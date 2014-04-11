34e8162253109cbfd6e93ea557b237319673c4f2
steve lamb <steve.lamb@bss.org>
2014-04-11 15:41:52 +0100

    an example of specifying static file paths

---
+++ flow/web/config.py
@@ -0,0 +1,6 @@
+# example config
+static_paths = [
+	{ "url_prefix": "assets", "local_path": "C:\\Development\\Personal\\flow\\static\\frontend\\assets" },
+	{ "url_prefix": "js", "local_path": "C:\\Development\\Personal\\flow\\static\\shared\\js" },
+	{ "url_prefix": "css", "local_path": "C:\\Development\\Personal\\flow\\static\\shared\\css" },
+]
