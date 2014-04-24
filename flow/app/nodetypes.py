550a9ed0ef9d0b85d1881ed4494aba34649d4042
stevel <sp61bd>
2014-04-24 19:34:29 +0100

    got filesystem walker walking..

--- flow/app/nodetypes.py
+++ flow/app/nodetypes.py
@@ -1,12 +1,46 @@
+import os
 """
 nodetypes
 """
 
 class NodeTypes:
-	def __init__():
-		pass
+	def __init__(self):
+		print "init NodeTypes..."
 
-	def rebuild():
+		self.definitions = []
+
+	def nodedefinition_processor(self, file_name):
+		print "nodedefinition_processor %s" % file_name
+
+	def nodeclass_processor(self, file_name):
+		print "nodeclass_processor %s" % file_name
+
+	def rebuild(self):
+		print "rebuild nodetypes..."
+		print "\n\nbuild nodedefs"
+		self.walk_directories(".nodedef", self.nodedefinition_processor)
+		print "\n\nbuild classes"
+		self.walk_directories(".py", self.nodeclass_processor)
+
+	def walk_directory(self, path, file_type, processor):
+		print "\n>> %s" % path
+		ext = file_type[:len(file_type)]
+
+		for file_name in os.listdir(path):
+			full_file_name = os.path.join(path, file_name)
+			if os.path.isdir(full_file_name):
+				self.walk_directory(full_file_name, file_type, processor)
+			if os.path.isfile(full_file_name):
+				if full_file_name.endswith(ext):
+					processor(full_file_name)
+
+
+
+	def walk_directories(self, file_type, processor):
+		root_path = "/Users/stevethehat/Development/flow/modules"
+		self.walk_directory(root_path, file_type, processor)
+
+		"""
 		test_dir = os.path.dirname(os.path.realpath(__file__))
 		print "run tests in '%s'...\n\n" % test_dir
 
@@ -25,4 +59,29 @@ class NodeTypes:
 							print test_member
 							function = getattr(test_module, test_name)
 							function(self)
+		"""
+
+os.system("cls")
+nt = NodeTypes()
+nt.rebuild()
 		
+
+"""
+class Singleton(object):
+	__instance = None
+
+	def __new__(cls):
+		if cls.__instance == None:
+			__instance = object.__new__(cls)
+			__instance.name = "the one"
+		return(__instance)
+
+
+test1 = Singleton()
+test2 = Singleton()
+
+print test1
+print test2
+
+
+"""
