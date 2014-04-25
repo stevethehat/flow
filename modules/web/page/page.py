088b691fd41c3ee8a829c21b5450a6f11aadaedb
stevel <sp61bd>
2014-04-25 12:01:25 +0100

    added page example test nodes

---
+++ modules/web/page/page.py
@@ -0,0 +1,14 @@
+from flow.app.nodetypes import *
+
+@NodeClass("page")
+class Page:
+	def __init__(self):
+		pass
+
+	@NodeAction("Edit")
+	def edit(self):
+		pass
+
+	@NodeAction("Edit Content")
+	def edit_content(self):
+		pass
