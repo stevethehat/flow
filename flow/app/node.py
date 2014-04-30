9d246e5c2d3cccb579ae494d9c79de722fa585cf
stevel <sp61bd>
2014-04-30 13:48:38 +0100

    Node.children method

--- flow/app/node.py
+++ flow/app/node.py
@@ -1,10 +1,18 @@
 from nodestore_file import NodeStore_File
 
 class Node:
-	def __init__(self, hierarchy, uid):
+	def __init__(self, hierarchy, uid, data = None):
 		self.hierarchy = hierarchy
 		self.uid = uid
-		self.data = self.hierarchy.store.get(uid)
+		if data != None:
+			self.data = data
+		else:
+			self.data = self.hierarchy.store.get(uid)
+
+	def children(self):
+		children = self.hierarchy.store.children(self.uid)
+		print "CHILDREN"
+		print children
 
 	def __repr__(self):
 		return(
