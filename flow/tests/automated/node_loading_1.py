79f9d3d0ded472e5417fc682d21a847fccc1694f
stevel <sp61bd>
2014-04-30 13:50:59 +0100

    children test

--- flow/tests/automated/node_loading_1.py
+++ flow/tests/automated/node_loading_1.py
@@ -1,9 +1,14 @@
 from flow.app.hierarchy import Hierarchy
+import shutil
 
 def test_1(flow):
+	
+	
 	print "load root node..."
 	root = flow.hierarchy.get_node("root")
 	print root
 
 	admin = flow.hierarchy.add_node("admin", "admin", parent_node = root)
 	print admin
+
+	children = root.children()
