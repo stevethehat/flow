1495767c5ab3acdb77c5e7ddc48f122e1b4af7c4
stevel <sp61bd>
2014-05-02 13:32:22 +0100

    added cached property decorator

--- flow/app/decorators.py
+++ flow/app/decorators.py
@@ -6,3 +6,12 @@ class BaseDecorator:
 			wrapped_f = f(*args)
 			return(wrapped_f)
 		return(wrapper)
+
+def cacheprop(fn):
+    attr_name = '_lazy_' + fn.__name__
+    @property
+    def _cacheprop(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+    return _cacheprop
