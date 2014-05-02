e09bef365a4c3c9906d8303f6d7e58b144c4fa3e
stevel <sp61bd>
2014-05-02 13:34:46 +0100

    had clear in not the best place :(

--- tests.py
+++ tests.py
@@ -2,11 +2,11 @@ import os
 from flow.app.flow import Flow
 from flow.tests.automated.node_loading_1 import *
 
+os.system("clear")
 flow = Flow(
 	{
 		"approot": os.path.dirname(os.path.realpath(__file__))
 	}
 )
 
-os.system("clear")
 test_1(flow)
