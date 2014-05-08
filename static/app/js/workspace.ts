15f0145d1b17596c092b9ff92937e53d84fb5951
steve lamb <steve.lamb@bss.org>
2014-05-08 12:47:43 +0100

    tests of server..

--- static/app/js/workspace.ts
+++ static/app/js/workspace.ts
@@ -1,5 +1,6 @@
 /// <reference path="jquery.d.ts" />
 /// <reference path="menus.ts" />
+/// <reference path="server.ts" />
 
 module Workspace{
     class Workspace {
@@ -9,18 +10,27 @@ module Workspace{
             var mainMenu: Menus.BarMenu = new Menus.BarMenu($('#mainMenu'));
             mainMenu.populate(
                 [
-                    {label: 'menu item1'},
-                    {label: 'hgjgjghjgjgj', icon: 'hello'},
-                    {label: 'sadadssa'}
+                    {label: 'menu item1', 'action': ''},
+                    {label: 'hgjgjghjgjgj', 'action': '', icon: 'hello'},
+                    {label: 'sadadssa', 'action': ''}
                 ]
             );
+            this.server = new Server.Server("localhost", "html");
+            this.server.get("1", "list", null, 
+                function(data){
+                    alert(data);
+                }
+            );
+
             alert('done init');
         }
         body: JQuery;
+        server: Server.Server;
         test() {
             return ('hello from test');
         }
     }
+
     $(document).ready(
         function(){
             var workspace: Workspace = new Workspace();        
