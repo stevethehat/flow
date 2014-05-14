c52e28efe7b72289a96bede6ca48d7a9ad796dd9
stevel <sp61bd>
2014-05-14 14:07:35 +0100

    added event handling to ListView

--- static/app/js/listings.ts
+++ static/app/js/listings.ts
@@ -16,6 +16,14 @@ module Listings{
     	items: ListItems;
     }
 
+    export interface ListSelect{
+    	(uid: string, listItem: JQuery): void;
+    }
+
+    export interface ListNavigate{
+    	(uid: string, listItem:JQuery): void;
+    }
+
 	export class ListView{
 		_workspace: Workspace.Workspace;
 		_container: JQuery;
@@ -24,7 +32,7 @@ module Listings{
 			this._container = container;
 		}
 
-		populate(list: List){
+		populate(list: List, selectEvent?: ListSelect, navigateEvent?: ListNavigate){
 			this._container.empty();
 			var ul: JQuery = $('<ul/>').addClass('list-group').appendTo(this._container);
 			for(var id in list.items){
@@ -32,8 +40,25 @@ module Listings{
 				var li: JQuery = $('<li/>')
 					.addClass('list-group-item')
 					.appendTo(ul);
+
+				if(selectEvent !== null){
+					li.click(
+						function(event){
+							event.stopPropagation();
+							selectEvent(item.uid, li);
+						}
+					);
+				}
+
+				var iconUrl: string = item.icon;
+				if(icon !== null){
+					iconUrl = this._workspace.iconPath + '/32x32/' + item.icon + '.png'
+				} else {
+					iconUrl = this._workspace.iconPath + '/32x32/book_spelling.png'
+				}
+
 				var icon: JQuery = $('<img/>')
-					.attr('src', '/assets/icons/32x32/book_spelling.png')
+					.attr('src', iconUrl)
 					.appendTo(li);
 				var description: JQuery = $('<span/>')
 					.text(item.description)
@@ -41,10 +66,22 @@ module Listings{
 				var navigate: JQuery = $('<span/>')
 					.addClass('pull-right list-nav')
 					.appendTo(li);
+
+				if(navigateEvent !== null){
+					navigate.click(
+						function(event){
+							event.stopPropagation();
+							navigateEvent(item.uid, li);
+						}
+					);
+				}
 				var navigateIcon: JQuery = $('<i/>').
 					addClass('fa fa-chevron-circle-right fa-lg pull-right')
 					.appendTo(navigate);
 			}
+		}
+		setSelected(li: JQuery){
+			alert('set selected');
 		}
 	}
 }
