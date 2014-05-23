0603165a137cda2c84e42a636d1a0eb8410f3f4e
stevel <sp61bd>
2014-05-23 10:45:16 +0100

    all sorts.. refactoring & test od => in callbacks

--- static/app/js/listings.ts
+++ static/app/js/listings.ts
@@ -17,15 +17,20 @@ module Listings{
     	items: ListItems;
     }
 
-    export interface ListSelect{
-    	(uid: string, listItem: JQuery): void;
+    export interface ViewEvent{
+    	(uid: string, element: JQuery): void;
     }
 
-    export interface ListNavigate{
-    	(uid: string, listItem:JQuery): void;
+    export interface ViewEventDefinition{
+    	eventName: string;
+    	event: ViewEvent;
     }
 
-	export class ListView{
+    export interface ViewEvents{
+    	[index: number]: ViewEventDefinition;
+    }
+
+    export class View{
 		_workspace: Workspace.Workspace;
 		_container: JQuery;
 		constructor(workspace: Workspace.Workspace, container: JQuery){
@@ -33,43 +38,49 @@ module Listings{
 			this._container = container;
 		}
 
-		populate(list: List, selectEvent?: ListSelect, navigateEvent?: ListNavigate){
+		setEvents(events?: ViewEvents): void{
+			alert('base setEvents');
+		}
+
+		populate(list: List, events?: ViewEvents){
 			var self: ListView = this;
 			this._container.empty();
 			list.iconPath = self._workspace.iconPath;
+
 			this._workspace.template.render('list', list,
 				function(html){
 					self._container.html(html);
-
-					self._container.find('li').click(
-						function(event){
-							event.stopPropagation();
-
-							var li: JQuery = $(event.target);
-							var uid: string = li.attr('data-flowuid');
-
-							self._container.find('li').removeClass('selected');
-
-							li.addClass('selected');
-							selectEvent(uid, li);
-						}
-					);
-
-					self._container.find('li span.list-nav').click(
-						function(event){
-							event.stopPropagation();
-
-							var li: JQuery = $(event.target).parent();
-							var uid: string = li.attr('data-flowuid');
-
-							self._container.find('li').removeClass('selected');
-
-							li.addClass('selected');
-							navigateEvent(uid, li);
-						}
-					);					
+					self.setEvents(events);
 				}
 			);
+		}		
+    }
+
+	export class ListView extends View{
+		setEvents(events?: ViewEvents): void{
+			this._container.find('li').click((event: JQueryEventObject) => {
+					event.stopPropagation();
+
+					var li: JQuery = $(event.target);
+					var uid: string = li.attr('data-flowuid');
+
+					this._container.find('li').removeClass('selected');
+
+					li.addClass('selected');
+				}
+			);
+
+			this._container.find('li span.list-nav').click((event: JQueryEventObject) => {
+					event.stopPropagation();
+
+					var li: JQuery = $(event.target).parent();
+					var uid: string = li.attr('data-flowuid');
+
+					this._container.find('li').removeClass('selected');
+
+					li.addClass('selected');
+				}
+			);					
 		}
 	}
 }
