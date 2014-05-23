30fc001e64614f2d6821fad0ec0b64d1db8ccd6e
stevel <sp61bd>
2014-05-23 13:11:15 +0100

    made build script more generic

--- static/app/js/buildjs.sh
+++ static/app/js/buildjs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
-tsc --out flowapp.js workspace.ts menus.ts dialogs.ts server.ts listings.ts templating.ts
-
+#tsc --out flowapp.js workspace.ts menus.ts dialogs.ts server.ts listings.ts templating.ts
+tsc --out flowapp.js *.ts
 echo
 echo Done..
 echo
